81cc6764645a8e04b772d55329adab152b4897c2
Dotan Simha <dotansimha@gmail.com>
2021-08-26 07:55:31 +0300

    step 12: added graphql to the http server

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,33 @@
 import fastify from "fastify";
+import { getGraphQLParameters, processRequest, Request } from "graphql-helix";
+import { schema } from "./schema";
 
 async function main() {
   const server = fastify();
 
-  server.get("/", (req, reply) => {
-    reply.send({ test: true });
+  server.post("/graphql", async (req, reply) => {
+    const request: Request = {
+      headers: req.headers,
+      method: req.method,
+      query: req.query,
+      body: req.body,
+    };
+
+    const { operationName, query, variables } = getGraphQLParameters(request);
+
+    const result = await processRequest({
+      request,
+      schema,
+      operationName,
+      query,
+      variables,
+    });
+
+    if (result.type === "RESPONSE") {
+      reply.send(result.payload);
+    } else {
+      reply.send({ error: "Stream not supported at the moment" });
+    }
   });
 
   server.listen(3000, "0.0.0.0", () => {
