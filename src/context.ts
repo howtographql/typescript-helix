c2a97d1cc391fab71fa96fdcfe14321ac750366a
Dotan Simha <dotansimha@gmail.com>
2021-09-19 14:37:59 +0300

    added pubSub to the GraphQL context

--- src/context.ts
+++ src/context.ts
@@ -1,12 +1,14 @@
 import { PrismaClient, User } from "@prisma/client";
 import { FastifyRequest } from "fastify";
 import { authenticateUser } from "./auth";
+import { pubSub } from "./pubsub";
 
 const prisma = new PrismaClient();
 
 export type GraphQLContext = {
   prisma: PrismaClient;
   currentUser: User | null;
+  pubSub: typeof pubSub;
 };
 
 export async function contextFactory(
@@ -15,5 +17,6 @@ export async function contextFactory(
   return {
     prisma,
     currentUser: await authenticateUser(prisma, request),
+    pubSub,
   };
 }
