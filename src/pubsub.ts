196e8946471af6471e2820b0824796d250b74ec2
Dotan Simha <dotansimha@gmail.com>
2021-09-19 16:34:05 +0300

    declare newVote event in pubsub

--- src/pubsub.ts
+++ src/pubsub.ts
@@ -1,9 +1,10 @@
-import { Link } from "@prisma/client";
+import { Link, Vote } from "@prisma/client";
 import { PubSub } from "graphql-subscriptions";
 import { TypedPubSub } from "typed-graphql-subscriptions";
 
 export type PubSubChannels = {
   newLink: [{ createdLink: Link }];
+  newVote: [{ createdVote: Vote }];
 };
 
 export const pubSub = new TypedPubSub<PubSubChannels>(new PubSub());
