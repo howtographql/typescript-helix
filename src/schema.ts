805eee28312ddfe77681d402a1f94464497f14c1
Dotan Simha <dotansimha@gmail.com>
2021-09-19 11:13:26 +0300

    added resolvers to feed query

--- src/schema.ts
+++ src/schema.ts
@@ -1,9 +1,29 @@
 import { makeExecutableSchema } from "@graphql-tools/schema";
 import typeDefs from "./schema.graphql";
 
+type Link = {
+  id: string;
+  url: string;
+  description: string;
+};
+
+const links: Link[] = [
+  {
+    id: "link-0",
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL",
+  },
+];
+
 const resolvers = {
   Query: {
-    info: () => "Test",
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links,
+  },
+  Link: {
+    id: (parent: Link) => parent.id,
+    description: (parent: Link) => parent.description,
+    url: (parent: Link) => parent.url,
   },
 };
 
