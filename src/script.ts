65b22c775d58c12d8e098f7ddd04ee3916a5a9f5
Dotan Simha <dotansimha@gmail.com>
2021-09-19 11:25:33 +0300

    added item creation to the prisma test script

--- src/script.ts
+++ src/script.ts
@@ -3,6 +3,12 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
+  const newLink = await prisma.link.create({
+    data: {
+      description: "Fullstack tutorial for GraphQL",
+      url: "www.howtographql.com",
+    },
+  });
   const allLinks = await prisma.link.findMany();
 
   console.log(allLinks);
