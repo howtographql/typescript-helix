e055520e01641b769ad8cae023d49cac860e585a
Dotan Simha <dotansimha@gmail.com>
2021-09-19 12:47:06 +0300

    Added authenticateUser function

--- src/auth.ts
+++ src/auth.ts
@@ -1,1 +1,20 @@
+import { PrismaClient, User } from "@prisma/client";
+import { FastifyRequest } from "fastify";
+import { JwtPayload, verify } from "jsonwebtoken";
+
 export const APP_SECRET = "this is my secret";
+
+export async function authenticateUser(
+  prisma: PrismaClient,
+  request: FastifyRequest
+): Promise<User | null> {
+  if (request.headers.authorization) {
+    const token = request.headers.authorization.split(" ")[1];
+    const tokenPayload = verify(token, APP_SECRET) as JwtPayload;
+    const userId = tokenPayload.userId;
+
+    return await prisma.user.findUnique({ where: { id: userId } });
+  }
+
+  return null;
+}
