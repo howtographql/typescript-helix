7613a9b472037aa3dfe0ecc05ed809b00cfda8e8
Dotan Simha <dotansimha@gmail.com>
2021-09-19 11:09:33 +0300

    added basic graphql handler to the server

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,44 @@
 import "graphql-import-node";
 import fastify from "fastify";
+import {
+  getGraphQLParameters,
+  processRequest,
+  Request,
+  sendResult,
+} from "graphql-helix";
+import { schema } from "./schema";
 
 async function main() {
   const server = fastify();
 
-  server.get("/", (req, reply) => {
-    reply.send({ test: true });
+  server.route({
+    method: "POST",
+    url: "/graphql",
+    handler: async (req, reply) => {
+      const request: Request = {
+        headers: req.headers,
+        method: req.method,
+        query: req.query,
+        body: req.body,
+      };
+
+      const { operationName, query, variables } = getGraphQLParameters(request);
+
+      const result = await processRequest({
+        request,
+        schema,
+        operationName,
+        query,
+        variables,
+      });
+
+      sendResult(result, reply.raw);
+    },
   });
 
-  server.listen(3000, "0.0.0.0");
+  server.listen(3000, "0.0.0.0", () => {
+    console.log(`Server is running on http://localhost:3000/`);
+  });
 }
 
 main();
