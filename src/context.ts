1324d062fc8377c59eb34bc595a658952750c313
Dotan Simha <dotansimha@gmail.com>
2021-09-19 12:06:19 +0300

    added prisma to graphql context

--- src/context.ts
+++ src/context.ts
@@ -1,3 +1,13 @@
-export async function contextFactory() {
-  return {};
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export type GraphQLContext = {
+  prisma: PrismaClient;
+};
+
+export async function contextFactory(): Promise<GraphQLContext> {
+  return {
+    prisma,
+  };
 }
