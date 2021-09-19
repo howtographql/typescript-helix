2f12db7c89376bfebb9f3dbe031063a163250ab6
Dotan Simha <dotansimha@gmail.com>
2021-09-19 11:11:19 +0300

    added graphiql

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
 import "graphql-import-node";
 import fastify from "fastify";
-import { getGraphQLParameters, processRequest, Request } from "graphql-helix";
+import {
+  getGraphQLParameters,
+  processRequest,
+  Request,
+  shouldRenderGraphiQL,
+  renderGraphiQL,
+} from "graphql-helix";
 import { schema } from "./schema";
 
 async function main() {
   const server = fastify();
 
   server.route({
-    method: "POST",
+    method: ["POST", "GET"],
     url: "/graphql",
     handler: async (req, reply) => {
       const request: Request = {
@@ -16,6 +22,17 @@ async function main() {
         query: req.query,
         body: req.body,
       };
+
+      if (shouldRenderGraphiQL(request)) {
+        reply.header("Content-Type", "text/html");
+        reply.send(
+          renderGraphiQL({
+            endpoint: "/graphql",
+          })
+        );
+
+        return;
+      }
 
       const { operationName, query, variables } = getGraphQLParameters(request);
 
