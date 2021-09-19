b3eff8b56f1a1cc413dc9b8bdcb8345a05e0a803
Dotan Simha <dotansimha@gmail.com>
2021-09-19 15:23:00 +0300

    added newLink event to the pubsub

--- src/pubsub.ts
+++ src/pubsub.ts
@@ -1,6 +1,9 @@
+import { Link } from "@prisma/client";
 import { PubSub } from "graphql-subscriptions";
 import { TypedPubSub } from "typed-graphql-subscriptions";
 
-export type PubSubChannels = {};
+export type PubSubChannels = {
+  newLink: [{ createdLink: Link }];
+};
 
 export const pubSub = new TypedPubSub<PubSubChannels>(new PubSub());
